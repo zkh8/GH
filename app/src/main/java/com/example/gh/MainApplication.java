d95fa641bdc108b4403d83527f9be95db7401c16
tao <Xt@19890815>
2022-03-09 11:59:00 +0800

    上传广告资源

--- app/src/main/java/com/example/gh/MainApplication.java
+++ app/src/main/java/com/example/gh/MainApplication.java
@@ -3,6 +3,7 @@ package com.example.gh;
 import android.app.Application;
 import android.util.Log;
 
+import com.anythink.core.api.ATSDK;
 import com.example.gh.bean.UserInfo;
 
 import java.util.UUID;
@@ -27,16 +28,23 @@ public class MainApplication extends Application {
     public String YZ_clientId = "302fab4a3ee5401887";
     public String YZ_appkey = "2481426c4c1742369552fc7e0f4b0a63";
     public int YZ_url_type = 0;
+    public static final String appid = "a5aa1f9deda26d";
+    public static final String appKey = "4f7b9ac17decb9babec83aac078742c7";
 
     @Override
     public void onCreate() {
         super.onCreate();
         mApp = this;
 
+        ATSDK.setNetworkLogDebug(true);//SDK日志功能，集成测试阶段建议开启，上线前必须关闭
+
+        ATSDK.init(this, appid, appKey);
+
+
         Log.d("MainApplication", "onCreate");
     }
 
-    public static MainApplication getInstance(){
+    public static MainApplication getInstance() {
 
         Log.d("MainApplication", "getInstance");
 
